efdd64e575f57eed245251e85edfd8238360c6e4
daydreamboy <wesley4chen@gmail.com>
2025-03-03 10:40:26 +0800

    HelloCocoaPods-Plugin: add code

--- HelloCocoaPods-Plugin/cocoapods-githooks/lib/cocoapods_plugin.rb
+++ HelloCocoaPods-Plugin/cocoapods-githooks/lib/cocoapods_plugin.rb
@@ -1,1 +1,12 @@
 require 'cocoapods-githooks/command'
+require_relative 'githooks-sync'
+
+module CocoapodsGitHooks
+  Pod::HooksManager.register('cocoapods-githooks', :post_install) do |context|
+    GitHooksSync.new.sync()
+  end
+
+  Pod::HooksManager.register('cocoapods-githooks', :post_update) do |context|
+    GitHooksSync.new.sync()
+  end
+end
